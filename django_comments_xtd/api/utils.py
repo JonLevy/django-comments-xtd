505392a157a35bdef3752474bee2884428235b4f
Jonathan Levy <jonl@advocatetax.com>
2020-05-29 18:32:36 -0400

    Convert ids to UUID before passing to filter.

--- django_comments_xtd/api/utils.py
+++ django_comments_xtd/api/utils.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from django.apps import apps
 from rest_framework.generics import get_object_or_404
 
@@ -7,8 +9,8 @@ def get_discussion_from_kwargs(kwargs, return_id):
     if set(kwargs.keys()) == {'item_pk', 'playlist_pk'}:
         disc = get_object_or_404(  # expect to throw this often
             Discussion,
-            item__id=kwargs['item_pk'],
-            playlist__id=kwargs['playlist_pk']
+            item__id=UUID(kwargs['item_pk']),
+            playlist__id=UUID(kwargs['playlist_pk'])
         )
         return return_id and str(disc.id) or disc
     elif set(kwargs.keys()) == {'content_type', 'object_pk'}:
